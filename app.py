62b2613e22622bdbf389707aeea18c825bd623e6
Alamin Ashraf <alaminashraf52@gmail.com>
2025-07-30 11:31:04 +0600

    Fix

--- app.py
+++ app.py
@@ -6,10 +6,15 @@ import pandas as pd
 from io import StringIO
 
 
-# =================================================
-if not spacy.util.is_package("en_core_web_sm"):
-    spacy.cli.download("en_core_web_sm")
-# ==============================================
+# ============================================================================
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+# ============================================================================
+
 
 # Load spaCy NLP model
 nlp = spacy.load("en_core_web_sm")
